4d618b45d461a05949ec2ebe089e1a8661ce2fa3
Mihail Pogosyan <m.pogosyan@asferro.com>
2020-02-18 01:05:11 +0200

    add some integation tests

--- src/app/control/control.component.spec.ts
+++ src/app/control/control.component.spec.ts
@@ -1,13 +1,107 @@
 import { ControlComponent } from './control.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { FormField, FormService } from '../services/form.service';
+import { By } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { DebugElement } from '@angular/core';
 
 describe('ControlComponent', () => {
   let component: ControlComponent;
+  let fixture: ComponentFixture<ControlComponent>;
 
   beforeEach(() => {
-    component = new ControlComponent(null);
+    TestBed.configureTestingModule({
+      declarations: [ ControlComponent ],
+      imports: [ HttpClientModule ],
+      providers: [ HttpClientModule, FormService ]
+    });
+    fixture = TestBed.createComponent(ControlComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create', () => {
+    component.form = new FormGroup({
+      test: new FormControl('', [])
+    });
+    component.field = {
+      id: 1,
+      isMultiple: false,
+      isGroup: false,
+      placeholder: 'Test placeholder',
+      code: 'test',
+      label: 'Test label',
+      inputType: 'text',
+      fields: [],
+      initialValue: '',
+      required: false,
+      validators: []
+    } as FormField;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
+  });
+
+  it('should display simple input without validation', async () => {
+    component.form = new FormGroup({
+      test: new FormControl('', [])
+    });
+    component.field = {
+      id: 1,
+      isMultiple: false,
+      isGroup: false,
+      placeholder: 'Test placeholder',
+      code: 'test',
+      label: 'Test label',
+      inputType: 'text',
+      fields: [],
+      initialValue: '',
+      required: false,
+      validators: []
+    } as FormField;
+    component.pathToRootForm = [];
+    fixture.detectChanges();
+    const de = fixture.debugElement;
+    const fieldsets: DebugElement[] = de.queryAll(By.css('fieldset'));
+    expect(fieldsets.length).toBe(1);
+    expect(de.query(By.css('input')).nativeElement.value).toBe('');
+    expect(de.query(By.css('input')).nativeElement.type).toBe('text');
+    expect(de.query(By.css('input')).nativeElement.placeholder).toContain('Test placeholder');
+    expect(de.query(By.css('label')).nativeElement.textContent).toContain('Test label');
+    expect(de.query(By.css('label')).nativeElement.textContent).not.toContain('*');
+  });
+  it('should display multiple input', () => {
+    component.form = new FormGroup({
+      test: new FormGroup({
+        input: new FormControl('', []),
+        values: new FormArray([])
+      })
+    });
+    component.field = {
+      id: 1,
+      isMultiple: true,
+      isGroup: false,
+      placeholder: 'Test placeholder',
+      code: 'test',
+      label: 'Test label',
+      inputType: 'text',
+      fields: [],
+      initialValue: '',
+      required: false,
+      validators: []
+    } as FormField;
+    component.pathToRootForm = [];
+    fixture.detectChanges();
+    const de = fixture.debugElement;
+    const fieldsets: DebugElement[] = de.queryAll(By.css('fieldset'));
+    expect(fieldsets.length).toBe(1);
+    expect(de.query(By.css('input')).nativeElement.value).toBe('');
+    expect(de.query(By.css('input')).nativeElement.type).toBe('text');
+    expect(de.query(By.css('input')).nativeElement.placeholder).toContain('Test placeholder');
+    expect(de.query(By.css('label')).nativeElement.textContent).toContain('Test label');
+    expect(de.query(By.css('label')).nativeElement.textContent).not.toContain('*');
+    expect(de.query(By.css('button[type=button]')).nativeElement).toBeDefined();
+    expect(de.query(By.css('button[type=button]')).nativeElement.textContent).toContain('Add value');
+    expect(de.query(By.css('ul')).nativeElement).toBeDefined();
+    expect(de.queryAll(By.css('li')).length).toBe(0);
   });
 });
--- src/app/form/form.component.spec.ts
+++ src/app/form/form.component.spec.ts
@@ -3,23 +3,24 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormComponent } from './form.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { FormService } from '../services/form.service';
+import { By } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ControlComponent } from '../control/control.component';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FormComponent ],
-      imports: [HttpClientModule],
-      providers: [FormService]
-    })
-    .compileComponents();
-  }));
-
+  let formService: FormService;
   beforeEach(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [ FormComponent, ControlComponent ],
+      imports: [HttpClientModule, FormsModule, ReactiveFormsModule ],
+      providers: [ FormService ]
+    });
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    formService = TestBed.get(FormService);
     fixture.detectChanges();
   });
 
@@ -28,14 +29,15 @@ describe('FormComponent', () => {
   });
 
   it('should call formService.init on init', () => {
-    const spy = spyOn(component.formService, 'init');
+    const spy = spyOn(formService, 'init');
     component.ngOnInit();
     expect(spy).toHaveBeenCalled();
   });
 
-  it('should call formService.submit on submit', () => {
-    const spy = spyOn(component.formService, 'submit');
-    component.onSubmit();
+  it('should call formService.submit on submit', async () => {
+    const spy = spyOn(formService, 'submit');
+    fixture.debugElement.query(By.css('form')).triggerEventHandler('submit', null);
+    await fixture.whenStable();
     expect(spy).toHaveBeenCalled();
   });
 });
